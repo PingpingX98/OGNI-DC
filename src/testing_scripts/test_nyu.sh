4c1677a65ea89caadae8201b23b02e73954044c4
PingpingX98 <18786681463@163.com>
2025-07-29 16:33:35 +0800

    nyu

--- src/testing_scripts/test_nyu.sh
+++ src/testing_scripts/test_nyu.sh
@@ -2,7 +2,6 @@ GRU_iters=5
 test_augment=0
 optim_layer_input_clamp=1.0
 depth_activation_format='exp'
-smaple=500
 ckpt=/home/descfly/Projects/OGNI-DC-main/src/checkpoints/NYU_best_performance.pt
 
 # for sample in 5 50 100 200 300 400 500 
@@ -12,7 +11,7 @@ ckpt=/home/descfly/Projects/OGNI-DC-main/src/checkpoints/NYU_best_performance.pt
 for noise_type in gaussian impulse rayleigh gamma exponential uniform
 do
 python main.py --dir_data /home/descfly/data/nyudepthv2 --data_name NYU --split_json ../data_json/nyu.json \
-    --gpus 0 --max_depth 10.0 --num_sample $sample \
+    --gpus 0 --max_depth 10.0 --num_sample 500 \
     --GRU_iters $GRU_iters --optim_layer_input_clamp $optim_layer_input_clamp --depth_activation_format $depth_activation_format \
     --test_only --test_augment $test_augment --pretrain $ckpt \
     --log_dir /data/compare/metric/OGNI-DC/experiments/ \

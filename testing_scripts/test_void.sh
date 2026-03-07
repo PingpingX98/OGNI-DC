2b45d9c6a889a6882023b821086b54cc84d44bf2
PingpingX98 <18786681463@163.com>
2026-03-07 12:37:04 +0800

    metric

--- testing_scripts/test_void.sh
+++ testing_scripts/test_void.sh
@@ -3,17 +3,16 @@ test_augment=0
 optim_layer_input_clamp=1.0
 depth_activation_format='exp'
 
-ckpt=/home/descfly/Projects/OGNI-DC-main/src/checkpoints/NYU_best_performance.pt
+ckpt=../checkpoints/NYU_best_performance.pt
 
-for sample in 150
+for sample in 150 500 1500
+
 do
-  python main.py --dir_data /home/descfly/data/void_release/void_${sample} --data_name VOID \
-    --gpus 0 --max_depth 5.0 \
+  python main.py --dir_data ../datas/void/void_${sample} --data_name VOID \
+    --gpus 0 --max_depth 10.0 \
     --GRU_iters $GRU_iters --optim_layer_input_clamp $optim_layer_input_clamp --depth_activation_format $depth_activation_format \
     --test_only --test_augment $test_augment --pretrain $ckpt \
-    --log_dir /data/compare/metric/OGNI-DC/experiments \
+    --log_dir ../experiments \
     --save "test_void${sample}" \
     --save_result_only
 done
-
-

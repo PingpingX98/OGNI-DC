7e8d827cc6417041a745145b52b791e2d2a5601c
PingpingX98 <18786681463@163.com>
2026-03-07 16:54:19 +0800

    metric

--- main.py
+++ main.py
@@ -40,6 +40,25 @@ from apex import amp
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
+def diff_level_noise_mul(x, mask=None, noise_level=1, seed=None):
+    import random
+    if mask is None:
+        mask = x > 0
+    # print(f"noise level is {noise_level}")
+    if seed is None:
+        seed = int(noise_level * 1000)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+    low = -noise_level
+    high = noise_level
+
+    uniform_noise = torch.empty_like(x).uniform_(low, high)
+    x_noisy = x.clone()
+    x_noisy[mask] = x[mask] * (1 + uniform_noise[mask])
+
+    return x_noisy
+
 # Minimize randomness
 def init_seed(seed=None):
     if seed is None:
@@ -472,6 +491,127 @@ def test(args):
     print('Elapsed time : {} sec, '
           'Average processing time : {} sec'.format(t_total, t_avg))
 
+
+def test_noisy(args):
+    # Prepare dataset
+    data = get_data(args)
+
+    data_test = data(args, 'test')
+
+    loader_test = DataLoader(dataset=data_test, batch_size=1,
+                             shuffle=False, num_workers=args.num_threads)
+
+    if args.model == 'OGNIDC':
+        loss = SequentialLoss(args)
+        # summ = OGNIDCSummary
+        summ_new = OGNIDCSummarynew
+    else:
+        raise NotImplementedError
+
+    # Network
+    if args.model == 'OGNIDC':
+        net = OGNIDC(args)
+    else:
+        raise TypeError(args.model, ['OGNIDC', ])
+    net.cuda()
+
+    if args.pretrain is not None:
+        assert os.path.exists(args.pretrain), \
+            "file not found: {}".format(args.pretrain)
+
+        checkpoint = torch.load(args.pretrain)
+        key_m, key_u = net.load_state_dict(checkpoint['net'], strict=False)
+
+        if key_u:
+            print('Unexpected keys :')
+            print(key_u)
+
+        if key_m:
+            print('Missing keys :')
+            print(key_m)
+            raise KeyError
+        print('Checkpoint loaded from {}!'.format(args.pretrain))
+
+    net = nn.DataParallel(net)
+
+    # metric = DCMetric(args)
+    metric_new = DCMetricnew(args)
+
+    # try:
+    #     os.makedirs(args.save_dir, exist_ok=True)
+    #     os.makedirs(args.save_dir + '/test', exist_ok=True)
+    # except OSError:
+    #     pass
+    try:
+        os.makedirs(args.log_dir, exist_ok=True)
+        os.makedirs(args.log_dir + '/test', exist_ok=True)
+    except OSError:
+        pass
+
+    # writer_test = summ(args.save_dir, 'test', args, None, metric.metric_name)
+    writer_test_new = summ_new(args.log_dir, 'test', args, None, metric_new.metric_name)
+
+    net.eval()
+
+    num_sample = len(loader_test) * loader_test.batch_size
+
+    pbar = tqdm(total=num_sample)
+
+    t_total = 0
+
+    init_seed()
+    for batch, sample in enumerate(loader_test):
+        sample = {key: val.cuda() for key, val in sample.items()
+                  if val is not None}
+        sample['dep'] = diff_level_noise_mul(sample['dep'], noise_level=args.noise_level)
+        if args.test_single:
+            if batch < 111:
+                continue
+            if batch > 111:
+                break
+        t0 = time.time()
+        with torch.no_grad():
+            output = net(sample)
+        t1 = time.time()
+
+        t_total += (t1 - t0)
+
+        if args.test_augment:
+            sample_fliplr = {key: torch.clone(val) for key, val in sample.items()
+                             if val is not None}
+            sample_fliplr['rgb'] = torch.flip(sample_fliplr['rgb'], (3,))
+            sample_fliplr['dep'] = torch.flip(sample_fliplr['dep'], (3,))
+
+            output_flip = net(sample_fliplr)
+            pred_flip_back = torch.flip(output_flip['pred'], (3,))
+
+            output['pred'] = (pred_flip_back + output['pred']) / 2.0
+
+        # metric_val = metric.evaluate(sample, output, 'test')
+        metric_val_new = metric_new.evaluate(sample, output, 'test')
+
+        # writer_test.add(None, metric_val)
+        writer_test_new.add(None, metric_val_new)
+
+        # # Save data for analysis
+        # writer_test.save(args.epochs, batch, sample, output)   # no using this format
+        if args.save_result_only:
+            writer_test_new.save(args.epochs, batch, sample, output)
+
+        current_time = time.strftime('%y%m%d@%H:%M:%S')
+        error_str = '{} | Test'.format(current_time)
+        if batch % args.print_freq == 0:
+            pbar.set_description(error_str)
+            pbar.update(loader_test.batch_size)
+
+    pbar.close()
+
+    # writer_test.update(args.epochs, sample, output)
+    writer_test_new.update(args.epochs, sample, output)
+
+    t_avg = t_total / num_sample
+    print('Elapsed time : {} sec, '
+          'Average processing time : {} sec'.format(t_total, t_avg))
 
 def main(args):
     init_seed()
